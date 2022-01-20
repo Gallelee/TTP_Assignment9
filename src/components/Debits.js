092095d18a1253115f01cd0cb34b1c976431079e
Gallelee <thimotus@gmail.com>
2022-01-19 22:47:04 -0500

    Add form to debits page

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -4,11 +4,20 @@ export default function Debits(props){
 
     let debitContents = props.debitInfo
 
+    const addDebit = (ev) => {
+       alert(ev.target[0].value)
+    }
 
     if(debitContents){
         return(
             <div>
                 <h1>Debits</h1>
+                <h2>Add new Debits?</h2>
+                <form onSubmit={(ev) => addDebit(ev)}>
+                    <input type={"text"} placeholder="Enter a description for your new Debit"/>
+                    <input type={"text"} placeholder="Enter the amount for the new debit"/>
+                    <input type={"submit"}/>
+                </form>
                 {debitContents.map( elm => {
                     return(<div className="debit-card">
                                 <h4>{elm.id}</h4>
